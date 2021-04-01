f1398b0c92a5d623c7929318d196d7bd70ebcf23
Cedric Delaruelle <cedric@delaruelle.eu>
2021-04-01 21:34:00 +0200

    Push dag 1

--- server/server.js
+++ server/server.js
@@ -6,5 +6,7 @@ console.log(path.resolve("client"));
 
 const server = express()
     .get("/", (req, res) => res.sendFile(path.resolve("client/html/index.html")))
+    .get("/nl", (req, res) => res.sendFile(path.resolve("client/html/versionDutch.html")))
+    .get("/eng", (req, res) => res.sendFile(path.resolve("client/html/versionEnglish.html")))
     .use(express.static(path.resolve("client")))
     .listen(PORT, () => console.log("Listening on localhost:" + PORT));
